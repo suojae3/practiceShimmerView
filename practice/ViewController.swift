cf517876444e750c317e06fabfeb0f7ad4ff34b0
suojae3 <suojae3@gmail.com>
2024-03-01 20:59:49 +0900

    연습연습연습

--- practice/ViewController.swift
+++ practice/ViewController.swift
@@ -1,19 +1,88 @@
-//
-//  ViewController.swift
-//  practice
-//
-//  Created by ㅣ on 3/1/24.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    let label = UILabel()
+    let gradientLayer = CAGradientLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setup()
+        layout()
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        gradientLayer.frame = label.bounds
+        gradientLayer.cornerRadius = label.bounds.height / 2
+    }
 }
 
+extension ViewController {
+    
+    func setup() {
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Shimmer"
+        label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 0.5)
+        label.layer.addSublayer(gradientLayer)
+        
+        let titleGroup = makeAnimationGroup()
+        titleGroup.beginTime = 0.0
+        gradientLayer.add(titleGroup, forKey: "backgroundColor")
+    }
+    
+    func layout() {
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor, multiplier: 1),
+            label.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: label.trailingAnchor, multiplier: 1),
+            label.heightAnchor.constraint(equalToConstant: 36),
+        ])
+    }
+    
+    func makeAnimationGroup(previousGroup: CAAnimationGroup? = nil) -> CAAnimationGroup {
+        let animDuration: CFTimeInterval = 1.5
+        let anim1 = CABasicAnimation(keyPath: #keyPath(CAGradientLayer.backgroundColor))
+        anim1.fromValue = UIColor.gradientLightGrey.cgColor
+        anim1.toValue = UIColor.gradientDarkGrey.cgColor
+        anim1.duration = animDuration
+        anim1.beginTime = 0.0
+
+        let anim2 = CABasicAnimation(keyPath: #keyPath(CAGradientLayer.backgroundColor))
+        anim2.fromValue = UIColor.gradientDarkGrey.cgColor
+        anim2.toValue = UIColor.gradientLightGrey.cgColor
+        anim2.duration = animDuration
+        anim2.beginTime = anim1.beginTime + anim1.duration
+
+        let group = CAAnimationGroup()
+        group.animations = [anim1, anim2]
+        group.repeatCount = .greatestFiniteMagnitude
+        group.duration = anim2.beginTime + anim2.duration
+        group.isRemovedOnCompletion = false
+
+        if let previousGroup = previousGroup {
+            group.beginTime = previousGroup.beginTime + 2.33
+        }
+
+        return group
+    }
+}
+
+extension UIColor {
+
+    static var gradientDarkGrey: UIColor {
+        return UIColor(red: 239 / 255.0, green: 200 / 255.0, blue: 241 / 255.0, alpha: 1)
+    }
+
+    static var gradientLightGrey: UIColor {
+        return UIColor(red: 201 / 255.0, green: 100 / 255.0, blue: 201 / 255.0, alpha: 1)
+    }
+}
